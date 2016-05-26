ef8b75099e5d7d9ddc49964d102fcd26c625fc59
Michael Shipley Barakat <michaebkats@gmail.com>
2016-05-25 18:55:08 -0700

    almost working in serveral places

--- app.js
+++ app.js
@@ -5,18 +5,52 @@ var right = document.getElementById('right');
 var displayButton = document.getElementById('chart-results');
 var resetButton = document.getElementById('reset-results');
 var playAgainButton = document.getElementById('play-again');
+var chartViews = document.getElementById('myChartViews');
+var chartClicks = document.getElementById('myChartClicks');
 var allProducts = [];
 var allPics = [];
 var allClicks = 0;
-var allClicksStored = 0;
+var maxClicks = 25;
 var picNames = ['bag', 'banana', 'bathroom', 'boots', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
-var parsedClicks = JSON.parse(localStorage.getItem('numberOfClicks'));
-var allClicksStored = parsedClicks;
-console.log(allClicksStored);
+var parsedProductItems = JSON.parse(localStorage.getItem('productData'));
+console.log(parsedProductItems);
 
-resetButton.style.display = 'none';
-displayButton.style.display = 'none';
-playAgainButton.style.display = 'none';
+function productAllClicks (){
+  var productAllClicks = [];
+  for (var i = 0; i < allProducts.length; i++){
+    productAllClicks.push(parsedProductItems[i].clicks);
+    console.log(productAllClicks);
+  }
+  return productAllClicks;
+};
+
+function productAllViews (){
+  var productAllViews = [];
+  for (var i = 0; i < allProducts.length; i++){
+    productAllViews.push(parsedProductItems[i].views);
+    //console.log(productAllViews);
+  }
+  return productAllViews;
+};
+
+function percentageOfClicks(clicks, views){
+  var percentage = clicks / views;
+  return percentage;
+}
+
+var clickPercentageData = function(){
+  var array1 = productAllViews();
+  // console.log(array1);
+  var array2 = productAllClicks();
+  // console.log(array2);
+  var arrayResult = [];
+  for (var i = 0; i < allProducts.length; i++){
+    var finalPercentage = percentageOfClicks(array2[i], array1[i]);
+    arrayResult.push(finalPercentage);
+  }
+  return arrayResult;
+  console.log(arrayResult);
+};
 
 function Product(name) {
   this.name = name;
@@ -29,43 +63,12 @@ for (var i = 0; i < picNames.length; i++){
   allProducts.push(new Product(picNames[i]));
 }
 
-// allProducts.push(this);
-//left.src = allProducts[15].path;
-
 function randNum (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function displayPics (){
-  if (allClicks < 25){
-    var leftIndex = randNum( 0 , allProducts.length);
-    left.src = allProducts[leftIndex].path;
-    allProducts[leftIndex].views += 1; // this adds to the object through the array
-    left.alt = allProducts[leftIndex].name;
-    console.log(allProducts[leftIndex].name + ' has been shown ' + allProducts[leftIndex].views + ' times');
-
-    var centerIndex = randNum( 0 , allProducts.length);
-    while (centerIndex === leftIndex) {
-      centerIndex = randNum(0 , allProducts.length);
-    }
-    center.src = allProducts[centerIndex].path;
-    allProducts[centerIndex].views += 1;
-    center.alt = allProducts[centerIndex].name;
-    console.log(allProducts[centerIndex].name + ' has been shown ' + allProducts[leftIndex].views + ' times');
-
-    var rightIndex = randNum( 0 , allProducts.length);
-    while (rightIndex === centerIndex || rightIndex === leftIndex) {
-      var rightIndex = randNum(0 , allProducts.length);
-    }
-    right.src = allProducts[rightIndex].path;
-    allProducts[rightIndex].views += 1;
-    right.alt = allProducts[rightIndex].name;
-    console.log(allProducts[centerIndex].name + ' has been shown ' + allProducts[leftIndex].views + ' times');
-  }
-}
-
-function displayPicsAgain (){
-  if (allClicks < 50){
+  if (allClicks < maxClicks){
     var leftIndex = randNum( 0 , allProducts.length);
     left.src = allProducts[leftIndex].path;
     allProducts[leftIndex].views += 1; // this adds to the object through the array
@@ -94,46 +97,47 @@ function displayPicsAgain (){
 
 function handlePicContainerClick (event){
   displayPics();
-  if (event.target.id === 'pic-container' && allClicks < 25 ){
+  if (event.target.id === 'pic-container' && allClicks < maxClicks ){
     return alert('click on a picture');
   }
   for (var i = 0; i < allProducts.length; i++){
-    if(event.target.alt === allProducts[i].name && allClicks < 25){
+    if(event.target.alt === allProducts[i].name && allClicks < maxClicks){
       allProducts[i].clicks += 1;
+
       console.log(allProducts[i].name + ' has ' + allProducts[i].clicks + ' clicks');
       allClicks += 1;
       console.log(allClicks);
-      localStorage.clear();
-      var allClicksStringified = JSON.stringify(allClicks);
-      localStorage.setItem('numberOfClicks', allClicksStringified);
+
+      // localStorage.clear();
+      // var allClicksStringified = JSON.stringify(allClicks);
+      // localStorage.setItem('numberOfClicks', allClicksStringified);
     }
   }
-  if (allClicks >= 25){
+  if (allClicks >= maxClicks){
     displayButton.style.display = 'inline-block';
     resetButton.style.display = 'inline-block';
-    playAgainButton.style.display = 'inline-block';
   }
-  console.log(event.target);
 
+  var allProductsStringged = JSON.stringify(allProducts);
+  localStorage.setItem('productData', allProductsStringged);
 }
 
-var ctx = document.getElementById('myChart');
-
-function renderChart(event){
+function renderChartClicks(){
+  var chartClicks = document.getElementById('myChartClicks');
   var data = {
     labels: [],
     datasets: [
       {
-        label: 'the most chosen products',
+        label: 'the most clicked products',
         data: []
       }
     ]
   };
-  if (allClicks < 25){
+  if (allClicks < maxClicks){
     alert('you have to choose another 25 products for busmall before you can see your results');
   }
 
-  if (allClicks >= 25){
+  if (allClicks >= maxClicks){
     for (var i = 0; i < picNames.length; i++){
       data.labels.push(picNames[i]);
       console.log(picNames[i]);
@@ -142,7 +146,7 @@ function renderChart(event){
     }
   }
 
-  var myChart = new Chart(ctx, {
+  var myChartClicks = new Chart(chartClicks, {
     type: 'bar',
     data: data,
     options: {
@@ -155,20 +159,80 @@ function renderChart(event){
       }
     }
   });
-  myChart.update();
 }
-function resetChart(event){
-  allClicks = 0;
-  var ctx = document.getElementById('myChart');
-  var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: [],
-      datasets: [{
-        label: 'the most chosen products',
+
+function renderChartViews(){
+  var chartViews = document.getElementById('myChartViews');
+  var data = {
+    labels: [],
+    datasets: [
+      {
+        label: 'the most viewed products',
         data: []
-      }]
-    },
+      }
+    ]
+  };
+  if (allClicks < maxClicks){
+    alert('you have to choose another 25 products for busmall before you can see your results');
+  }
+
+  if (allClicks >= maxClicks){
+    for (var i = 0; i < picNames.length; i++){
+      data.labels.push(picNames[i]);
+      console.log(picNames[i]);
+      data.datasets[0].data.push(parsedProductItems[i].views);
+      console.log(parsedProductItems[i].views);
+    }
+  }
+
+  var myChartViews = new Chart(chartViews, {
+    type: 'bar',
+    data: data,
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          }
+        }]
+      }
+    }
+  });
+  myChartViews.update();
+}
+
+function renderChartPercentage(){
+  var chartPercentage = document.getElementById('myChartPercentage');
+  var data = {
+    labels: [],
+    datasets: [
+      {
+        label: 'percentage of clicks per product',
+        data: []
+      }
+    ]
+  };
+  if (allClicks < maxClicks){
+    alert('you have to choose another 25 products for busmall before you can see your results');
+  }
+
+  if (allClicks >= maxClicks){
+    for (var i = 0; i < picNames.length; i++){
+      data.labels.push(picNames[i]);
+      console.log(picNames[i]);
+      data.datasets[0].data.push(clickPercentageData[i]);
+      console.log(clickPercentageData[i]);
+    }
+  }
+
+  // data.labels.push(picNames[i]);
+  // console.log(picNames[i]);
+  // data.datasets[0].data.push(parsedProductItems[i].views);
+  // console.log(parsedProductItems[i].views);
+
+  var myChartPercentage = new Chart(chartPercentage, {
+    type: 'bar',
+    data: data,
     options: {
       scales: {
         yAxes: [{
@@ -181,13 +245,77 @@ function resetChart(event){
   });
 }
 
+function resetCharts(event){
+  var chartViews = document.getElementById('myChartViews');
+  var chartClicks = document.getElementById('myChartClicks');
+  allClicks = 0;
+  var data = {
+    labels: [],
+    datasets: [
+      {
+        label: 'the most clicked products',
+        data: []
+      }
+    ]
+  };
+  var myChartClicks = new Chart(chartClicks, {
+    type: 'bar',
+    data: data,
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          }
+        }]
+      }
+    }
+  });
+  var data = {
+    labels: [],
+    datasets: [
+      {
+        label: 'the most clicked products',
+        data: []
+      }
+    ]
+  };
+  var myChartViews = new Chart(chartViews, {
+    type: 'bar',
+    data: data,
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          }
+        }]
+      }
+    }
+  });
+  myChartClicks.clear();
+  myChartViews.clear();
+  chartClicks.parentNode.removeChild(chartClicks);
+  chartViews.parentNode.removeChild(chartViews);
+  chartPercentage.parentNode.removeChild(chartPercentage);
+  return false;
+}
+
+function renderCharts(event){
+  renderChartViews();
+  renderChartClicks();
+  clickPercentageData();
+  renderChartPercentage();
+}
+
 displayPics();
 
 function playAgain (){
   displayPicsAgain();
 }
 
+resetButton.style.display = 'none';
+displayButton.style.display = 'none';
 picContainer.addEventListener('click', handlePicContainerClick);
-displayButton.addEventListener('click', renderChart);
-resetButton.addEventListener('click', resetChart);
-playAgainButton.addEventListener('click', playAgain);
+displayButton.addEventListener('click', renderCharts);
+resetButton.addEventListener('click', resetCharts);
